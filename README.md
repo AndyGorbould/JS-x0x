936541d77d7058589f856d26f4dba5007b3ec43b
Andy Gorbould <gorbould@mail.com>
2022-06-24 18:39:13 +0200

    row functional

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ The goal is to create a JavaScript version of the Roland TR series of drum machi
 useful resources:
 https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API - Web Audio API
 https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API - Using the Web Audio API
+https://bashooka.com/coding/web-audio-javascript-libraries/ - a few JS audio APIs that could be used
 
 
 notes:
 lay out a 16x1 grid (16x td, 1x tr), make a loop & light up the correct step, make 1 grid per sound!
+buttons; getElementByTag, then foreach to give them an ID ?
 
 
 ideas:
--- script.js
+++ script.js
@@ -7,26 +7,36 @@
 
 
 // create an instance of the audio context
-var AudioContext = window.AudioContext || window.webkitAudioContext;
+// var AudioContext = window.AudioContext || window.webkitAudioContext;
 
-var audioContext = new AudioContext();
+// var audioContext = new AudioContext();
 
 
 
 // where are the samples?
-var audioFileURL = "./audio/TR909/BT0A0A7.WAV";
+// var audioFileURL = "./audio/TR909/BT0A0A7.WAV";
 
 
-// create a sequencer row
+// create a sequencer row, 16 cells with buttons inside
 function createRow() {
     const body = document.getElementById("target"); // where is is going
-    let tbl = document.createElement("table"); // make a table
-    let tr = document.createElement("tr"); // make a table
-  
+    let tr = document.createElement("tr"); // make a row
+
     for (let i = 0; i < 16; i++) {
+        const step = document.createElement("input");
+        step.id = "step-" + i;
+        step.className = "step";
+        step.type = "button"
         const td = tr.insertCell();
+        td.appendChild(step);
     }
     body.appendChild(tr);
-  }
-  
-  createRow();
+
+    
+}
+
+createRow();
+
+
+let allSteps = document.querySelectorAll(".step");
+console.log(allSteps);
